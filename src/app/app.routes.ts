35226faa6b0b7549ad5c4a2d0dff7ccda06625dc
Adrian Kuznar <pospolight@gmail.com>
2024-08-29 13:25:45 +0200

    install and config prettier, material, proxy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,19 @@
-import { Routes } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { ErrorComponent } from './shared/layouts/error/error.component';
+import { Routes } from "@angular/router";
+import { ErrorComponent } from "./shared/layouts/error/error.component";
+import { LoginComponent } from "./login/login.component";
 
 export const routes: Routes = [
   {
-    path: '',
-    component: WelcomeComponent,
+    path: "",
+    component: LoginComponent,
   },
   {
-    path: 'files',
-    loadChildren: () =>
-      import('./files-manager/files.routes').then((r) => r.FILES_ROUTES),
+    path: "files",
+    loadChildren: () => import("./files-manager/files.routes").then((r) => r.FILES_ROUTES),
   },
   {
-    path: '**',
-    pathMatch: 'full',
+    path: "**",
+    pathMatch: "full",
     component: ErrorComponent,
   },
 ];
