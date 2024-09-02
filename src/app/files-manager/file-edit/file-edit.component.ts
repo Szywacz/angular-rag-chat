78926a38132158b660962b32f56635de94339437
Adrian Kuznar <pospolight@gmail.com>
2024-09-02 12:03:10 +0200

    breadcrumbs component test, test-styled

--- src/app/files-manager/file-edit/file-edit.component.ts
+++ src/app/files-manager/file-edit/file-edit.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BreadcrumbsComponent } from '@components/breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-file-edit',
   standalone: true,
-  imports: [],
+  imports: [BreadcrumbsComponent],
   templateUrl: './file-edit.component.html',
-  styleUrl: './file-edit.component.scss'
+  styleUrl: './file-edit.component.scss',
 })
 export class FileEditComponent {}
