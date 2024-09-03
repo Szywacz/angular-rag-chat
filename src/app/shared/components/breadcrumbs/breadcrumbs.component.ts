6c37f9ba9554fd941c3b1ce41a9971a85f0657a4
Adrian Kuznar <pospolight@gmail.com>
2024-09-03 16:28:45 +0200

    reactive breadcrumbs based on url

--- src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,12 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
+
+interface Breadcrumb {
+  label: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -10,12 +16,38 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './breadcrumbs.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class BreadcrumbsComponent {
-  constructor(private router: Router) {}
+export class BreadcrumbsComponent implements OnInit {
+  breadcrumbs: Breadcrumb[] = [];
 
   navItems: string[] = this.router.url.slice(1).split('/');
 
-  concatenatePreviousRoutes(i: number) {
-    return this.navItems.slice(0, i + 1).join('/');
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      this.generateBreadcrumbs();
+    });
+    this.generateBreadcrumbs();
+  }
+
+  private generateBreadcrumbs() {
+    let route = this.activatedRoute.root ? this.activatedRoute.root : null;
+    let url = '';
+    this.breadcrumbs = [];
+
+    while (route) {
+      if (route.routeConfig && route.routeConfig.path && route.routeConfig.path !== '') {
+        const path: string = route.routeConfig.path;
+        const label = decodeURIComponent(path).toUpperCase();
+        url += `/${path}`;
+
+        this.breadcrumbs.push({ label, url });
+      }
+
+      route = route.firstChild;
+    }
   }
 }
